f36db77f49dd736c5f7af43295faeb004e296bff
andela-oduyile <oluwatomi.duyile@andela.com>
2017-07-18 01:57:32 +0100

    add devise gem and add routes

--- db/migrate/20170713142518_create_notes.rb
+++ db/migrate/20170713142518_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration[5.1]
   def change
     create_table :notes do |t|
       t.string :title
-      t.description :content
+      t.text :content
 
       t.timestamps
     end
