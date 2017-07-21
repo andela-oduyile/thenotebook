56abdd5054d63b184f7166238e585fed401225a0
andela-oduyile <oluwatomi.duyile@andela.com>
2017-07-21 21:49:23 +0100

    completed index page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'welcome#index'
-
   resources :notes
+  authenticated :user do
+    root 'notes#index', as: 'authenticated_root'
+  end
+  root 'welcome#index'
 end
+ 
