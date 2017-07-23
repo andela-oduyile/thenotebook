3c1f607810431561811050159401560ff259dcdb
andela-oduyile <oluwatomi.duyile@andela.com>
2017-07-23 20:08:40 +0100

    add search feature

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,11 @@
 class Note < ApplicationRecord
   belongs_to :user
+
+  def self.search(user_id, search)
+    if search
+      where(:user_id => user_id).where(["title LIKE ?", "%#{search}%"])
+    else
+      where(:user_id => user_id)
+    end
+  end
 end
