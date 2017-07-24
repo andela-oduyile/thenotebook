4c352ebb311b5c2c4a4d0e182102f89e993a811c
andela-oduyile <oluwatomi.duyile@andela.com>
2017-07-24 11:43:30 +0100

    make links active

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,9 +3,9 @@ class Note < ApplicationRecord
 
   def self.search(user_id, search)
     if search
-      where(:user_id => user_id).where(["title LIKE ?", "%#{search}%"])
+      where(user_id: user_id).where(['title LIKE ?', '%#{search}%'])
     else
-      where(:user_id => user_id)
+      where(user_id: user_id)
     end
   end
 end
